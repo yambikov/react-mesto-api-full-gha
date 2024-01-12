b105e368dbee526f3ed1de3b26525f4f03862d0c
yambikov <yambikov@yandex.ru>
2024-01-13 00:18:27 +0300

    34 PR-15 ready for first review

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ const app = express();
 // const { PORT } = process.env;
 // const PORT = 3000;
 
-// app.use(cors());
+// / app.use(cors());
 app.use(cors({
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
